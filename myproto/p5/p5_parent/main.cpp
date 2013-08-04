7199851a270912de03203616bd8510443142231c
adrian orozco <orozcoadrian@gmail.com>
2013-08-04 02:12:02 +0000

    wired up car_lib as dep

--- myproto/p5/p5_parent/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    std::cout << "p5" << std::endl;
-    return 0;
-}
--- myproto/p5/p5_parent/p5_app/main.cpp
+++ myproto/p5/p5_parent/p5_app/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "person.pb.h"
+#include "car.pb.h"
 
 int main()
 {
     tutorial::Person person;
+    tutorial::Car car;
     std::cout << "p5" << std::endl;
     return 0;
 }
